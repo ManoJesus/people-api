c050a52631eac6502e0f2c635a63cf5526a3f5eb
ManoJesus <lucas_748l@hotmail.com>
2021-12-23 10:17:29 -0300

    Trying to set up heroku

--- src/main/java/com/github/manojesus/peopleapidio/respositories/PersonRepository.java
+++ src/main/java/com/github/manojesus/peopleapidio/respositories/PersonRepository.java
@@ -1,6 +1,6 @@
 package com.github.manojesus.peopleapidio.respositories;
 
-import com.github.manojesus.peopleapi.entities.Person;
+import com.github.manojesus.peopleapidio.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person,Long> {
